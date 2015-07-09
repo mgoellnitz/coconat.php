a5d4299f49d1597bd40b898822c99f07addbeeaa
Martin Goellnitz <martin@provocon.eu>
2015-07-09 23:48:18 +0200

    Comment typo

--- src/coconat/internal/CoconatBlob.php
+++ src/coconat/internal/CoconatBlob.php
@@ -1,22 +1,22 @@
 <?php
 
 /*
- * 
+ *
  * Copyright 2015 Martin Goellnitz
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 namespace coconat\internal;
@@ -24,7 +24,7 @@ namespace coconat\internal;
 use coconat\Blob;
 
 /**
- * Single implementation of the Blob interface.
+ * Simple implementation of the Blob interface.
  */
 class CoconatBlob implements Blob {
 
